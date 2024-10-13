649225cd50f5cd0d7c8e10963863482168b4cd4d
Tom <tombrandisicloud@gmail.com>
2024-10-13 14:52:22 +0100

    add website files, voting, message removal, cookie
    i didn't really do proper git commits for this but it works so...
    the css be better
    need to check if the html is accessible

--- database.go
+++ database.go
@@ -3,11 +3,14 @@ package main
 import (
 	"database/sql"
 	"log"
+	"strconv"
+	"time"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 type Message struct {
+	Id       int
 	Contents string
 	Name     string
 	Website  string
@@ -15,7 +18,7 @@ type Message struct {
 }
 
 func connectToDatabase() *sql.DB {
-	database, _ := sql.Open("sqlite3", "./messages.db")
+	database, _ := sql.Open("sqlite", "./messages.db")
 	err := database.Ping()
 	if err != nil {
 		log.Println(err)
@@ -32,7 +35,9 @@ func makeTable(db *sql.DB) {
 		content TEXT NOT NULL,
 		name TEXT,
 		website TEXT,
-		email TEXT
+		email TEXT,
+		upvotes INT,
+		time INT
     );`)
 
 	log.Println("made table if not exist")
@@ -42,21 +47,81 @@ func makeTable(db *sql.DB) {
 	}
 }
 
+func removeLowest(db *sql.DB) {
+	query := `
+    SELECT COUNT(*) FROM Messages
+    ;`
+	response, err := db.Query(query)
+	if err != nil {
+		log.Println(err)
+	}
+	var rowsStr string
+	for response.Next() {
+		response.Scan(&rowsStr)
+	}
+
+	rows, _ := strconv.Atoi(rowsStr)
+	log.Println(rowsStr)
+	if rows >= 10 {
+
+		log.Println("removing lowest")
+		query = `
+		DELETE FROM Messages WHERE id = (SELECT id FROM Messages ORDER BY upvotes ASC, time ASC LIMIT 1)
+		;`
+		_, err = db.Exec(query)
+
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+}
+
 func addMessage(db *sql.DB, content string, name string, website string, email string) {
+	removeLowest(db)
 	log.Println("adding message")
 	query := `
-    INSERT INTO Messages (content, name, website, email) VALUES (?, ?, ?, ?)
+    INSERT INTO Messages (content, name, website, email, upvotes, time) VALUES (?, ?, ?, ?, 0, ?)
     ;`
-	_, err := db.Exec(query, content, name, website, email)
+	_, err := db.Exec(query, content, name, website, email, time.Now().Unix())
 
 	if err != nil {
 		log.Println(err)
 	}
 }
 
+func addVote(db *sql.DB, id int) {
+	log.Println("adding vote")
+	query := `
+    UPDATE Messages SET upvotes = upvotes + 1 WHERE id=?
+    ;`
+	_, err := db.Exec(query, id)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func getName(db *sql.DB, id int) (string, error) {
+	log.Println("selecting name")
+	query := `
+	SELECT name FROM Messages WHERE id=?
+	;`
+	response, err := db.Query(query, id)
+	if err != nil {
+		log.Println(err)
+	}
+	var name string
+	for response.Next() {
+		response.Scan(&name)
+	}
+
+	return name, err
+}
+
 func getMessages(db *sql.DB) []Message {
 	query := `
-	SELECT content, name, website, email FROM Messages
+	SELECT id, content, name, website, email FROM Messages ORDER BY upvotes ASC, time ASC
 	;`
 	response, err := db.Query(query)
 	if err != nil {
@@ -65,7 +130,7 @@ func getMessages(db *sql.DB) []Message {
 	var Messages []Message
 	for response.Next() {
 		var Message Message
-		response.Scan(&Message.Contents, &Message.Name, &Message.Website, &Message.Email)
+		response.Scan(&Message.Id, &Message.Contents, &Message.Name, &Message.Website, &Message.Email)
 		Messages = append(Messages, Message)
 	}
 	return Messages
--- wall.go
+++ wall.go
@@ -5,9 +5,17 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type Pagedata struct {
+	Messages         []Message
+	Vote             string
+	SubmittedMessage bool
+}
 
 var db *sql.DB
 
@@ -20,7 +28,8 @@ func main() {
 
 	fs := http.FileServer(http.Dir("./website"))
 
-	r.HandleFunc("/", handleAddMessage).Methods("POST")
+	r.HandleFunc("/add", handleAddMessage).Methods("POST")
+	r.HandleFunc("/vote", handleVote).Methods("POST")
 	r.HandleFunc("/", displayIndex).Methods("GET")
 
 	r.PathPrefix("/").Handler(fs)
@@ -30,16 +39,64 @@ func main() {
 }
 
 func handleAddMessage(w http.ResponseWriter, r *http.Request) {
+	_, cookie_err := r.Cookie("message-submitted") // cookie_err should NOT be nil to add message
+	if cookie_err != nil {
+		addMessage(db, r.FormValue("Contents"), r.FormValue("Name"), r.FormValue("Website"), r.FormValue("Email"))
+		messageCookie := &http.Cookie{Name: "message-submitted", Value: "true", HttpOnly: false, Expires: time.Now().Add(24 * time.Hour)}
+		http.SetCookie(w, messageCookie)
 
-	addMessage(db, r.FormValue("Contents"), r.FormValue("Name"), r.FormValue("Website"), r.FormValue("Email"))
-	displayIndex(w, r)
+	} else {
+		println("already submitted message")
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+func handleVote(w http.ResponseWriter, r *http.Request) {
+	vote, err := strconv.Atoi(r.FormValue("Vote"))
+	_, cookie_err := r.Cookie("vote") // cookie_err should NOT be nil to vote
+	if err == nil && cookie_err != nil {
+		addVote(db, vote)
+		voteCookie := &http.Cookie{Name: "vote", Value: r.FormValue("Vote"), HttpOnly: false, Expires: time.Now().Add(24 * time.Hour)}
+		http.SetCookie(w, voteCookie)
+
+	} else {
+		println("invalid vote")
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 
 }
 
 func displayIndex(w http.ResponseWriter, r *http.Request) {
+	var pagedata Pagedata
+
+	voteCookie, voteCookie_err := r.Cookie("vote")
 	messages := getMessages(db)
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	err := tmpl.Execute(w, messages)
+	if voteCookie_err == nil {
+		vote, convert_err := strconv.Atoi(voteCookie.Value)
+		if convert_err != nil {
+			log.Println(convert_err)
+		}
+
+		name, name_err := getName(db, vote)
+		if name_err != nil {
+			log.Println(name_err)
+		}
+
+		pagedata.Vote = name
+	}
+
+	_, messageCookie_err := r.Cookie("message-submitted")
+	if messageCookie_err != nil { // there is NOT a cookie
+		pagedata.SubmittedMessage = false
+	} else {
+		pagedata.SubmittedMessage = true
+	}
+
+	pagedata.Messages = messages
+	tmpl := template.Must(template.ParseFiles("website/templates/index.html"))
+	err := tmpl.Execute(w, pagedata)
 	if err != nil {
 		log.Println(err)
 	}
